ee93701d1b6da209b546e2935aed7cddcfeb554d
Artem Ivlev <fordfocus2goga@gmail.com>
2021-11-27 20:41:10 +0400

    the solution of the problem "lesson_7_task_5"

--- lesson_7/5_th/lesson_7_task_5.py
+++ lesson_7/5_th/lesson_7_task_5.py
@@ -1,29 +1,37 @@
-# *(вместо 4) Написать скрипт, который выводит статистику для заданной папки в виде словаря,
-# в котором ключи те же, а значения — кортежи вида (<files_quantity>,
-# [<files_extensions_list>]), например:
-# {
-# 100: (15, ['txt']),
-# 1000: (3, ['py', 'txt']),
-# 10000: (7, ['html', 'css']),
-# 100000: (2, ['png', 'jpg'])
-# }
-# Сохраните результаты в файл <folder_name>_summary.json в той же папке, где запустили скрипт.
 import os
+from collections import defaultdict
+import json
+'''
+Нужно создать словарь в котором кортеж из количества файлов и списка расширений
+'''
 
-size_stats = {}
-for item in os.scandir(r'C:\Users\Артём\PycharmProjects'):
-    if item.is_file():
-        size = item.stat().st_size
-        threshold = 10 ** len(str(size))
-        ext = item.name.split('.')[-1]
-        try:
-            size_stats[threshold][0] += 1
-            ext_list = size_stats[threshold][1]
-            print(size_stats)
-            if ext in ext_list:
-                pass
-            else:
-                ext_list.append(ext)
-        except (KeyError, IndexError):
-            size_stats[threshold] = [1, [ext]]
-print(size_stats, 1)
+def create_dict_files(root_dir):
+    test_dict = defaultdict(list)
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            file_size = os.stat(os.path.join(root, file)).st_size
+            ext = file.split('.')[-1]
+            range_size = 10 ** len(str(file_size))
+            test_dict[range_size].append(ext)
+    return result_dict(test_dict)
+
+
+def result_dict(args):
+    tuple_dict = {}
+    for range_size, ext in sorted(args.items()):
+        list_ext = set(ext)
+        tuple_dict[range_size] = (len(ext), list(list_ext))
+    return tuple_dict
+
+
+def create_json_file(path_file, result):
+    with open(path_file, 'w', encoding='utf-8') as f:
+        json.dump(result, f)
+
+
+if __name__ == '__main__':
+    root_dir = input('Какую папку будем просматривать?: ')
+    foler_name = root_dir.split(os.sep)[-1]
+    result_json_file = os.path.join(root_dir, foler_name + 'result.json')
+    create_dict_files(root_dir)
+    create_json_file(result_json_file, create_dict_files(root_dir))
